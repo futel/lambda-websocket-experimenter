104cc1de2f628b807a0701205ec6a6039e9efd64
Karl Anderson <kra@monkey.org>
2024-10-24 22:51:19 -0700

    draft

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from boto3.session import Session
-from chalice import Chalice
-from chalice import WebsocketDisconnectedError
+import chalice
+#from chalice import Chalice
+#from chalice import WebsocketDisconnectedError
 
 from chalicelib import env_util
 from chalicelib import sns_client
@@ -8,7 +9,7 @@ from chalicelib import util
 
 env = env_util.get_env()
 
-app = Chalice(app_name='experimenter')
+app = chalice.Chalice(app_name='experimenter')
 app.websocket_api.session = Session()
 app.experimental_feature_flags.update([
     'WEBSOCKETS'
@@ -25,18 +26,6 @@ def foo():
   <Pause length="40"/>
 </Response>"""
 
-def _announce(event):
-    # Announce presence.
-    #sns_client.publish(event.connection_id, env)
-    #env['table'].put_item(
-    #    Item={"operation": "connect", "connection_id": event.connection_id})
-    env['table'].put_item(
-        #TableName="lambda_websocket_experimenter",
-        #Item={
-        #    "operation": "connect",
-        #    "connection_id": event.connection_id})
-        Item={"operation": "connect", "connection_id": event.connection_id})
-
 # {'Items': [{'connection_id': 'AIyPpcY0vHcCJgQ=', 'operation': 'connect'}, {'connection_id': 'AIzKXfJQPHcCGDA=', 'operation': 'connect'}, {'connection_id': 'bar', 'operation': 'connect'}, {'connection_id': 'foo', 'operation': 'connect'}], 'Count': 4, 'ScannedCount': 4, 'ResponseMetadata': {'RequestId': 'HV490E4VHA6UIMN7RI76BTDF37VV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Thu, 24 Oct 2024 04:24:02 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '296', 'connection': 'keep-alive', 'x-amzn-requestid': 'HV490E4VHA6UIMN7RI76BTDF37VV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '3712998993'}, 'RetryAttempts': 0}}
 def _connections():
     items = env['table'].scan()
@@ -44,11 +33,27 @@ def _connections():
     items = [i['connection_id'] for i in items]
     return items
 
+def _broadcast(event):
+    connection_ids = _connections()
+    for connection_id in connection_ids:
+        try:
+            app.websocket_api.send(
+                connection_id=connection_id,
+                message=event.body)
+        except chalice.WebsocketDisconnectedError:
+            util.log("broadcast removing disconected {}".format(connection_id))
+            util.remove_connection(connection_id, env)
+        except chalice.BadRequestError:
+            util.log("broadcast removing bad {}".format(connection_id))
+            util.remove_connection(connection_id, env)
+        except Exception as e:
+            util.log("broadcast unknown exception {}".format(e))
+
 @app.on_ws_connect()
 def connect(event):
     util.log("connect")
     util.log(event.to_dict())
-    _announce(event)
+    util.announce_connection(event.connection_id, env)
 
 @app.on_ws_disconnect()
 def disconnect(event):
@@ -59,17 +64,7 @@ def disconnect(event):
 def message(event):
     util.log("message")
     util.log(event.to_dict())
-    connection_ids = _connections()
-    util.log(connection_ids)
-    for connection_id in connection_ids:
-        try:
-            app.websocket_api.send(
-                connection_id=connection_id,
-                message=event.body)
-        except Exception as e:
-            # we expect WebsocketDisconnectedError
-            # BadRequestException
-            util.log(e)
+    _broadcast(event)
 
 # @app.on_sns_message(topic='lambda_websocket_experimenter')
 # def event_handler(event):
--- chalicelib/util.py
+++ chalicelib/util.py
@@ -1,20 +1,36 @@
-def get_instance(env):
-    # We could use the host header in the request instead?
-    return env['stage']
+# def get_instance(env):
+#     # We could use the host header in the request instead?
+#     return env['stage']
 
 def log(msg):
     print(msg)
 
-def log_request(request):
-    msg = 'request '
-    msg += 'query_params:{} '.format(request.query_params)
-    msg += 'uri_params:{} '.format(request.uri_params)
-    msg += 'path:{} '.format(request.path)
-    msg += 'raw_body:{}'.format(request.raw_body)
-    log(msg)
+# def log_request(request):
+#     msg = 'request '
+#     msg += 'query_params:{} '.format(request.query_params)
+#     msg += 'uri_params:{} '.format(request.uri_params)
+#     msg += 'path:{} '.format(request.path)
+#     msg += 'raw_body:{}'.format(request.raw_body)
+#     log(msg)
 
-def log_response(response):
-    msg = 'response '
-    msg += 'status_code:{} '.format(response.status_code)
-    msg += 'body:{}'.format(response.body)
-    log(msg)
+# def log_response(response):
+#     msg = 'response '
+#     msg += 'status_code:{} '.format(response.status_code)
+#     msg += 'body:{}'.format(response.body)
+#     log(msg)
+
+
+def announce_connection(connection_id, env):
+    #sns_client.publish(event.connection_id, env)
+    #env['table'].put_item(
+    #    Item={"operation": "connect", "connection_id": event.connection_id})
+    env['table'].put_item(
+        #TableName="lambda_websocket_experimenter",
+        #Item={
+        #    "operation": "connect",
+        #    "connection_id": event.connection_id})
+        Item={"operation": "connect", "connection_id": connection_id})
+
+def remove_connection(connection_id, env):
+    env['table'].delete_item(
+        Key={'connection_id': connection_id})
